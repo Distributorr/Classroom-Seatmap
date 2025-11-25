267f785f1d6de758e9c30bfd485a1f076d7450bd
Distributorr <148499730+Distributorr@users.noreply.github.com>
2025-11-25 11:13:10 +0100

    Update app.js
    Darkmode

--- app.js
+++ app.js
@@ -777,4 +777,24 @@ function escapeHtml(s = '') {
 
 })();
 
+// Read toggle and apply on load
+const toggle = document.getElementById('darkModeToggle');
+const savedMode = localStorage.getItem('theme');
+
+if (savedMode === 'dark') {
+  document.body.classList.add('dark');
+  toggle.checked = true;
+}
+
+// Listener for toggle
+toggle.addEventListener('change', () => {
+  if (toggle.checked) {
+    document.body.classList.add('dark');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.remove('dark');
+    localStorage.setItem('theme', 'light');
+  }
+});
+
 
